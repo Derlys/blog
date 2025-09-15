848b377c938b41f09d14bb61365073832c235d8b
Derlys Dominguez <derlys275@gmail.com>
2025-09-15 15:34:05 +0200

    fix: force to post type frontmatter

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import { Post } from "@/types/post";
+import { Post, Frontmatter } from "@/types/post";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
@@ -9,23 +9,25 @@ export function getPostSlugs(): string[] {
     return fs.readdirSync(postsDirectory);
 }
 
-export async function getPostBySlug(slug: string): Promise<Post> {
+export function getPostBySlug(slug: string): Post {
     const realSlug = slug.replace(/\.md$/, "");
     const fullPath = path.join(postsDirectory, `${realSlug}.md`);
-    const fileContents = await fs.promises.readFile(fullPath, "utf8");
+    const fileContents = fs.readFileSync(fullPath, "utf8");
     const { data, content } = matter(fileContents);
+
+    // aquí forzamos a que data sea de tipo Frontmatter
+    const frontmatter = data as Frontmatter;
 
     return {
         slug: realSlug,
-        frontmatter: data as Post["frontmatter"],
+        frontmatter,
         content,
     };
 }
 
-export async function getAllPosts(): Promise<Post[]> {
+export function getAllPosts(): Post[] {
     const slugs = getPostSlugs();
-    const posts = await Promise.all(slugs.map((slug) => getPostBySlug(slug)));
-
+    const posts = slugs.map((slug) => getPostBySlug(slug));
     return posts.sort(
         (a, b) =>
             new Date(b.frontmatter.date).getTime() -
